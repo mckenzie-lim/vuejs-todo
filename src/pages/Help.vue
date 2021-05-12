9b84665eb1c321095c330174212eaecd50ec84a5
McKenzie Lim <mckenzie.haejin.lim@gmail.com>
2021-05-12 21:11:44 +0200

    "done"

---
+++ src/pages/Help.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page class="q-pa-lg">
+  <h5 class="q-mt-none">Help</h5>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit, ducimus! Nobis consectetur laudantium neque nihil animi repudiandae autem ipsa non ipsam, deleniti iusto. Natus alias reiciendis iure rerum quia sed.</p>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id deleniti laboriosam quis fuga voluptates iure possimus dolore provident ullam? Ipsa exercitationem blanditiis sunt, maxime aut ipsum deserunt perferendis. Rerum, velit.</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+
+}
+</script>
